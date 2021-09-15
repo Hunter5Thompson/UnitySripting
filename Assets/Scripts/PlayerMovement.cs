5c2f33543b6279a83bee71bbddd507c559ec334a
strosche <robert.scheike@student.hs-anhalt.de>
2021-09-15 14:30:41 +0200

    Prefab System ( respawn System must be fixed . The devil is in the code of SpawnManager. Maybe a issue with the connection in unity.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public float jumpForce = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *=gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
