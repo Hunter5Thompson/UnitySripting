2f1d9755e357af3234dc6233326b3cf571c2fe3a
strosche <robert.scheike@student.hs-anhalt.de>
2021-09-17 13:51:04 +0200

    Added a particleSystems and Sound to the character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody playerRb;
 
     private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    
+    public ParticleSystem dirtParticle; 
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public float gravityModifier;
     public bool isOnGround = true;
     public float jumpForce = 10;
@@ -17,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *=gravityModifier;
     }
 
@@ -28,6 +35,8 @@ public class PlayerMovement : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -36,12 +45,16 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play(); 
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
